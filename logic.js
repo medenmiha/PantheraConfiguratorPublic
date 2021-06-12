bcd2cb6989be860a39f3c90268ca7b6d2ff9f013
Miha Meden <meden.miha@gmail.com>
2021-06-12 16:24:02 +0200

    1

--- logic.js
+++ logic.js
@@ -297,6 +297,25 @@ animate();
   //var target =  { x : 0, y: 0, z: 0 };
   //const tween = new TWEEN.Tween(position).to(target, 2000); //
   //tween.start();
+  
+  var slideIndex = 1;
+  showDivs(slideIndex);
+  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
+  }
+
 
 
 
