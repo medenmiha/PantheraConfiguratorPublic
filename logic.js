1645331315ad433fa13920b5b9bb4cbea238bbba
Miha Meden <meden.miha@gmail.com>
2021-06-16 08:40:17 +0200

    1

--- logic.js
+++ logic.js
@@ -224,7 +224,7 @@ function exteriorConfiguration(){
 function seatsConfiguration(){
 
   action1.clampWhenFinished = true;
-  action1.loop = THREE.LoopOnce;
+  action1.loop = THREE.LoopPingPong;
   action1.play();
 
   action2.clampWhenFinished = true;
